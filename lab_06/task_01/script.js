c240f0297db1ec6ae0649a1a7cb84fa2ae7f3427
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2022-04-17 10:14:17 +0500

    Completed Lab6 Task1
    Completed Lab6 Task1

---
+++ lab_06/task_01/script.js
@@ -0,0 +1,8 @@
+function calculatePower(num) {
+  console.log(`Power of 100 to number ${num}\tis :`, 100 ** num);
+}
+
+calculatePower(5);
+calculatePower(10);
+calculatePower(20);
+calculatePower(100);
