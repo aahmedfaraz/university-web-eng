3acdbed8f4b77114d35c570cfdb514317c511627
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2022-04-22 15:44:27 +0500

    Completed Lab07 Task04
    Completed Lab07 Task04

---
+++ lab_07/task_04/script.js
@@ -0,0 +1,6 @@
+const allNumbers = [
+  1, 11, 2, 22, 3, 33, 4, 44, 5, 55, 6, 66, 7, 77, 8, 88, 9, 99,
+];
+
+console.log(`The Array    : ${allNumbers}`);
+console.log(`Filtered <10 : ${allNumbers.filter((num) => num > 10)}`);
