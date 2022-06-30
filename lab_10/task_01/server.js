3d30ced4e6552ddfceb349be6333a03046e3262e
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2022-06-30 16:46:02 +0500

    Completed Lab 10 - Task 01

--- lab_10/task_01/server.js
+++ lab_10/task_01/server.js
@@ -4,28 +4,6 @@ const server = http.createServer((req, res) => {
     if(req.method === 'GET' && req.url === '/'){
         res.write('Greetings: Good Morning')
         res.end()
-    } else if (req.method === 'GET' && req.url === '/posts'){
-        console.log(['post1', 'post2', 'post3', 'post4']);
-        console.log(JSON.stringify(['post1', 'post2', 'post3', 'post4']));
-        res.write(JSON.stringify(['post1', 'post2', 'post3', 'post4']))
-        res.end()
-    } else if (req.method === 'GET' && req.url === '/credentials'){
-        console.log({
-            name: "ahmed faraz",
-            email: "ahmedfaraz@gmail.com",
-            password: "12345"
-        });
-        console.log(JSON.stringify({
-            name: "ahmed faraz",
-            email: "ahmedfaraz@gmail.com",
-            password: "12345"
-        }));
-        res.write(JSON.stringify({
-            name: "ahmed faraz",
-            email: "ahmedfaraz@gmail.com",
-            password: "12345"
-        }))
-        res.end()
     }
 })
 
